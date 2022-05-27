04283ef37c5dd5785ece88fc56f88677476894e2
nejcfzohar <47608726+nejcfzohar@users.noreply.github.com>
2022-05-27 16:30:58 +0200

    Update .gitignore

--- resources/views/calendar_events.blade.php
+++ resources/views/calendar_events.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>DeweSoft Assignment</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('laravel_logo.png') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
 
     <script type="text/javascript" src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
  
